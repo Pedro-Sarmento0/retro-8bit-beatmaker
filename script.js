e372c21ca3ef153f83d521db3060bd290668ec26
Pedro Sarmento TI01TA <pedro_sarmento0@hotmail.com>
2026-08-01 00:43:04 -0300

    add laser, coin, and jump sound effects
    Expanded the playRetroSound function to include oscillator logic for the laser, coin, and jump sounds using the Web Audio API.

--- script.js
+++ script.js
@@ -21,5 +21,40 @@ function playRetroSound(soundType) {
             osc.type = 'sine';
             osc.frequency.setValueAtTime(150, now);
             osc.frequency.exponentialRampToValueAtTime(0.01, now + 0.3);
+            gain.gain.setTargetAtTime(1, now);
+            gain.gain.exponentialRampToValueAtTime(0.01, now + 0.3);
+            osc.start(now);
+            osc.stop(now + 0.3);
+            break;
+
+        case 'laser': //arcade spaceship laser shot
+            osc.type = 'sawtooth';
+            osc.frequency.setTargetAtTime(880, now);
+            osc.frequency.exponentialRampToValueAtTime(110, now + 0.2);
+            gain.gain.setValueAtTime(0.5, now);
+            gain.gain.exponentialRampToValueAtTime(0.01, now + 0.2);
+            osc.start(now);
+            osc.stop(now + 0.2);
+            break;
+            
+        case 'coin': //Classic arcade coin sound
+            osc.type = 'square';
+            osc.frequency.setValueAtTime(987.77, now);
+            osc.frequency.setValueAtTime(1318.51, now + 0.08);
+            gain.gain.setValueAtTime(0.3, now);
+            gain.gain.exponentialRampToValueAtTime(0.01, now + 0.35);
+            osc.start(now);
+            osc.stop(now + 0.35);
+            break;
+
+        case 'jump': //8-bit platformer jump
+            osc.type = 'square';
+            osc.frequency.setValueAtTime(150, now);
+            osc.frequency.exponentialRampToValueAtTime(600, now + 0.2);
+            gain.gain.setValueAtTime(0.3, now);
+            gain.gain.exponentialRampToValueAtTime(0.01, now + 0.2);
+            osc.start(now);
+            osc.stop(now + 0.2);
+            break;
     }
 }
